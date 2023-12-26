6ab52204e861b1c799a85a6c83f128233487b491
Brent Cleary <brent.cleary@gmail.com>
2023-12-26 15:09:07 -0800

    Email Service Complete

--- ContactPro/Controllers/ContactsController.cs
+++ ContactPro/Controllers/ContactsController.cs
@@ -13,6 +13,7 @@ using ContactPro.Models.ViewModels;
 using ContactPro.Enums;
 using ContactPro.Services;
 using ContactPro.Services.Interfaces;
+using Microsoft.AspNetCore.Identity.UI.Services;
 
 namespace ContactPro.Controllers
 {
@@ -22,26 +23,30 @@ namespace ContactPro.Controllers
         private readonly UserManager<AppUser> _userManager;
         private readonly IImageService _imageService;
         private readonly IAddressBookService _addressBookService;
+        private readonly IEmailSender _emailService;
 
 
         public ContactsController(ApplicationDbContext context, 
                                   UserManager<AppUser> userManager, 
                                   IImageService imageService,
-                                  IAddressBookService addressBookService)
+                                  IAddressBookService addressBookService,
+                                  IEmailSender emailService)
         {
             _context = context;
             _userManager = userManager;
             _imageService = imageService;
             _addressBookService = addressBookService;
+            _emailService = emailService;
         }
 
         // GET: Contacts
         [Authorize]
-        public IActionResult Index(int categoryId)
+        public IActionResult Index(int categoryId, string swalMessage = null)
         {
-            
+            ViewData["SwalMessage"] = swalMessage;
+
             var contacts = new List<Contact>();
-            string appUserId = _userManager.GetUserId(User);
+            string? appUserId = _userManager.GetUserId(User);
 
             // return UserId and it's associated contacts and categories
             AppUser? appUser = _context.Users
@@ -134,10 +139,32 @@ namespace ContactPro.Controllers
                 EmailData = emailData
             };
 
-
             return View(model);
         }
 
+
+        // Post: Contact Send Email Action
+        [HttpPost]
+        [Authorize]
+        public async Task<IActionResult> EmailContact(EmailContactViewModel ecvm)
+        {
+
+            if(ModelState.IsValid)
+            {
+                try
+                {
+                    await _emailService.SendEmailAsync(ecvm.EmailData.EmailAddress, ecvm.EmailData.Subject, ecvm.EmailData.Body);
+                    return RedirectToAction("Index", "Contacts", new { swalMessage = "Success: Email Sent!"});
+                }
+                catch
+                {
+                    return RedirectToAction("Index", "Contacts", new { swalMessage = "Error: Email Send Failed!" });
+                    throw;
+                }
+            }
+            return View(ecvm);
+
+        }
 
 
         // GET: Contacts/Details/5
