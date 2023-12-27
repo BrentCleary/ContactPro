4975741896ce8ce177606c653eab5289f1859eb0
Brent Cleary <brent.cleary@gmail.com>
2023-12-26 17:17:40 -0800

    Category Field Populates by Category and User Id

--- ContactPro/Controllers/CategoriesController.cs
+++ ContactPro/Controllers/CategoriesController.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Authorization;
 using ContactPro.Data;
 using ContactPro.Models;
+using ContactPro.Models.ViewModels;
 
 
 namespace ContactPro.Controllers
@@ -16,18 +17,26 @@ namespace ContactPro.Controllers
     public class CategoriesController : Controller
     {
         private readonly ApplicationDbContext _context;
+        private readonly UserManager<AppUser> _userManager;
 
-        public CategoriesController(ApplicationDbContext context)
+        public CategoriesController(ApplicationDbContext context, 
+                                    UserManager<AppUser> userManager)
         {
             _context = context;
+            _userManager = userManager;
         }
 
         // GET: Categories
         [Authorize]
         public async Task<IActionResult> Index()
         {
-            var applicationDbContext = _context.Categories.Include(c => c.AppUser);
-            return View(await applicationDbContext.ToListAsync());
+            string? appUserId = _userManager.GetUserId(User);
+
+            var categories = await _context.Categories.Where(c => c.AppUserId == appUserId)
+                                        .Include(c => c.AppUser)
+                                        .ToListAsync();
+
+            return View(categories);
         }
 
         // GET: Categories/Details/5
@@ -84,12 +93,16 @@ namespace ContactPro.Controllers
                 return NotFound();
             }
 
-            var category = await _context.Categories.FindAsync(id);
+            string appUserId = _userManager.GetUserId(User);
+
+            var category = await _context.Categories.Where(c => c.Id == id && c.AppUserId == appUserId)
+                                                    .FirstOrDefaultAsync();
+
             if (category == null)
             {
                 return NotFound();
             }
-            ViewData["AppUserId"] = new SelectList(_context.Users, "Id", "Id", category.AppUserId);
+
             return View(category);
         }
 
