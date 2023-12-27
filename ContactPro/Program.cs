249e031cf4542ef36ac4f31e00d728517c9f6035
Brent Cleary <brent.cleary@gmail.com>
2023-12-27 06:49:15 -0800

    Email Environment Setup to Deploy to Hiroku

--- ContactPro/Program.cs
+++ ContactPro/Program.cs
@@ -5,6 +5,7 @@ using Microsoft.EntityFrameworkCore;
 using ContactPro.Services;
 using ContactPro.Services.Interfaces;
 using Microsoft.AspNetCore.Identity.UI.Services;
+using ContactPro.Helpers;
 
 
 var builder = WebApplication.CreateBuilder(args);
@@ -12,7 +13,9 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 //var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 
-var connectionString = builder.Configuration.GetSection("pgSettings")["pgConnection"];
+// var connectionString = builder.Configuration.GetSection("pgSettings")["pgConnection"];
+var connectionString = ConnectionHelper.GetConnectionString(builder.Configuration);
+
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 	options.UseNpgsql(connectionString));
@@ -29,7 +32,13 @@ builder.Services.AddScoped<IEmailSender, EmailService>();
 
 builder.Services.Configure<MailSettings>(builder.Configuration.GetSection("MailSettings"));
 
+
 var app = builder.Build();
+var scope = app.Services.CreateScope();
+
+// get the database update with the latest migrations
+await DataHelper.ManageDataAsync(scope.ServiceProvider);
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
